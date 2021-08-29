6d77bcaa7279d79a68415b905002a58802018ee5
pinn.public <tlsqhrud98@gmail.com>
2021-08-30 00:03:52 +0900

    보경커밋

--- RoutineMaker/src/main/java/com/routinemaker/challenge/ChallengeDAO.java
+++ RoutineMaker/src/main/java/com/routinemaker/challenge/ChallengeDAO.java
@@ -1,8 +1,17 @@
 package com.routinemaker.challenge;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ChallengeDAO {
 
+	private SqlSessionTemplate template;
+
+	public List<ChallengeDTO> getOriginalList() {
+		
+		return template.selectList("calendar.getOriginalList");
+	}
 }
